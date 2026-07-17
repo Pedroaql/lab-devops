3612b623ad52defb46904d4b596605ef724878fc
Pedro <pedro.ribe@hotmail.com>
2026-07-16 21:21:13 -0300

    fix: corrige IP real, sintaxe do backend s3 e restaura gitignore

--- terraform-projeto/backend.tf
+++ terraform-projeto/backend.tf
@@ -1,16 +1,13 @@
-# Credenciais NÃO ficam aqui. São lidas via variáveis de ambiente:
-#   export AWS_ACCESS_KEY_ID="<access key do minio>"
-#   export AWS_SECRET_ACCESS_KEY="<secret key do minio>"
-
 terraform {
   backend "s3" {
     bucket                      = "terraform-state"
     key                         = "lab/terraform.tfstate"
     region                      = "us-east-1"
-    endpoint                    = "http://<IP_DO_POP_OS>:9000"
+    endpoints                   = { s3 = "http://192.168.0.11:9000" }
     skip_credentials_validation = true
+    skip_requesting_account_id  = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
-    force_path_style            = true
+    use_path_style              = true
   }
 }
--- terraform-projeto/variables.tf
+++ terraform-projeto/variables.tf
@@ -1,5 +1,5 @@
 variable "infra_host" {
   description = "IP do nó de infraestrutura (Pop!_OS) na rede local"
   type        = string
-  default     = "<IP_DO_POP_OS>"
+  default     = "192.168.0.11"
 }
